073436f6f2eb5c817eac41ca389cc8a5b7f0fac6
Deimos <deimoshall@gmail.com>
2023-02-02 22:45:41 -0600

    feat: validate the options

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,21 @@ fn main() {
     generate_password(false, false, true, 16);
 }
 
-fn generate_password(hasNumbers: bool, hasCapLetters: bool, hasSymbols: bool, lenght: u8) -> String {
+fn generate_password(has_numbers: bool, has_cap_letters: bool, has_symbols: bool, lenght: u8) -> String {
     let symbols_list = vec!['!', '#', '$'];
     let numbers_list: Vec<char> = (b'0'..=b'9').map(|x| x as char).collect();
     let cap_letters_list: Vec<char> = (b'A'..=b'Z').map(|x| x as char).collect();
     let mut possible_chars: Vec<char> = (b'a'..=b'z').map(|x| x as char).collect();
-    println!("{:?}", numbers_list);
+
+    if has_numbers {
+        possible_chars.extend(numbers_list.iter().cloned());
+    }
+    if has_cap_letters {
+        possible_chars.extend(cap_letters_list.iter().cloned());
+    }
+    if has_symbols {
+        possible_chars.extend(symbols_list.iter().cloned());
+    }
+
     String::from("")
 }
